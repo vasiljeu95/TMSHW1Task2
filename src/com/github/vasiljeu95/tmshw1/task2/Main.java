466664717224b1fcfc9235e456b4fc74324d0f06
vasiljeu95 <vasilyeu95@gmail.com>
2021-11-29 23:37:14 +0300

    Initial commit

---
+++ src/com/github/vasiljeu95/tmshw1/task2/Main.java
@@ -0,0 +1,38 @@
+package com.github.vasiljeu95.tmshw1.task2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if (num >= 0 && num <= 6) {
+            switch (num) {
+                case 1:
+                    System.out.println("one");
+                    break;
+                case 2:
+                    System.out.println("two");
+                    break;
+                case 3:
+                    System.out.println("three");
+                    break;
+                case 4:
+                    System.out.println("four");
+                    break;
+                case 5:
+                    System.out.println("five");
+                    break;
+                case 6:
+                    System.out.println("six");
+                    break;
+                default:
+                    System.out.println("invalid");
+            }
+        } else {
+            System.out.println("Error! This number is incorrect!");
+        }
+    }
+}
+
